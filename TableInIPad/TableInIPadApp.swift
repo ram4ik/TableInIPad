2b810ffa7d28bbf4162b65948c29c24d427169c1
Ramill <ramill_ibragimov@icloud.com>
2022-10-02 11:52:53 +0300

    Initial Commit

---
+++ TableInIPad/TableInIPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableInIPadApp.swift
+//  TableInIPad
+//
+//  Created by Ramill Ibragimov on 02.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TableInIPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
